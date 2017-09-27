a4d4a9152de40cee48254a7b64ce2fb0fe9ae436
Bondarchuk97Ilja <Bondarchuk97Ilja@github.com>
2017-09-27 21:00:55 +0300

    binary_search

--- src/com/lessons/binary_search/Main.java
+++ src/com/lessons/binary_search/Main.java
@@ -4,20 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] array = {14, 12, 56, 48, 10, 1, 11, 54, 88, 15};
-        int n;
-        int a, b, c;
-        n = array.length;
-        for (int j = 0; j <= n; j++) {
-            for (int i = 0; i < n - 1; i++) {
-                a = array[i];
-                b = array[i + 1];
-                if (a > b) {
-                    array[i] = b;
-                    array[i + 1] = a;
-                }
+        int[] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+        int left, right, mid;
+        left = array [0];
+        right = array[array.length - 1];
+        int x = 5;
+
+        for (int i = 0; i<(array.length/2);i++) {
+            mid = (left+right)/2;
+            if (x == mid){
+                System.out.println(i);
+                break;
             }
+            if (x > mid) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+
         }
-        System.out.println(array[0] + " " + array[1] + " " + array[2] + " " + array[3] + " " + array[4] + " " + array[5] + " " + array[6] + " " + array[7] + " " + array[8] + " " + array[9]);
     }
 }
