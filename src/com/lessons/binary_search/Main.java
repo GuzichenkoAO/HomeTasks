363e67afb46927aa631f97d4b240e2e163b1b106
Bondarchuk97Ilja <Bondarchuk97Ilja@github.com>
2017-10-02 21:20:32 +0300

    binary_search - last update

--- src/com/lessons/binary_search/Main.java
+++ src/com/lessons/binary_search/Main.java
@@ -6,22 +6,27 @@ public class Main {
 
         int[] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
         int left, right, mid;
-        left = array [0];
-        right = array[array.length - 1];
-        int x = 5;
-
-        for (int i = 0; i<(array.length/2);i++) {
+        left = 0;
+        right = array.length - 1;
+        int x = 10;
+        for (int i = 0; i<array.length;i++) {
             mid = (left+right)/2;
-            if (x == mid){
-                System.out.println(i);
-                break;
-            }
-            if (x > mid) {
+            if (x > array[mid]) {
                 left = mid;
-            } else {
+            }
+            if (x < array[mid]) {
                 right = mid;
             }
+            if (x == array[mid]){
+                System.out.println(mid);
+                break;
+            }
 
+            if (i == (left + (right-left)/2)){
+                System.out.println("Нет числа в массиве");
+                break;
+            }
         }
+
     }
 }
